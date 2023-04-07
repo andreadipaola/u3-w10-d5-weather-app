92738b3ca6e486b81fedfabad1651a4cfeced8f9
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-04-07 10:57:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="outline-custom-dark">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
